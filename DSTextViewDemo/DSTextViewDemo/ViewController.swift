f76606df3fd3d2d377d2120229636cc2688d04e8
Dream <396587868@qq.com>
2020-12-15 17:45:39 +0800

    2020-12-15 17:45:43, commit probject

---
+++ DSTextViewDemo/DSTextViewDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DSTextViewDemo
+//
+//  Created by Dream on 2020/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ DSTextViewDemo/Pods/DSBase/DSBase/Code/DSBase.swift
@@ -0,0 +1,28 @@
+//
+//  DSBase.swift
+//  DSBase
+//
+//  Created by Dream on 2020/12/8.
+//
+
+public struct DS<DSBase> {
+    public let ds: DSBase
+    public init(_ ds : DSBase) {
+        self.ds = ds
+    }
+}
+
+public protocol DSCompatible { }
+
+public extension DSCompatible {
+    
+    var ds: DS<Self> {
+        set { }
+        get { DS(self) }
+    }
+    
+    static var ds: DS<Self>.Type {
+        set { }
+        get { DS<Self>.self }
+    }
+}
